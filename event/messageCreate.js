5bc1dc11a59136f8d04fb2dd83ef753367f1b2cf
antoinemcx <antoniodu1333@gmail.com>
2024-11-23 20:58:12 +0100

    ⚡️ Code readability and useless returns

--- event/messageCreate.js
+++ event/messageCreate.js
@@ -14,51 +14,51 @@ module.exports = async (bot, message) => {
     lang_query = await db.get(`lang_${message.guild.id}`);
     bot.language = require(`../language/${lang_query === null ? default_lang : lang_query.lang}.js`);
 
-    // BOT MENTION
-    if(message.content.match(new RegExp(`^<@!?${bot.user.id}>( |)$`))) {
-        message.channel.send(bot.language.MENTION_BOT.replace(/<name>/g, bot.user.username).replace(/<prefix>/g, prefix));
-        return;
-    }
+    if(message.content.match(new RegExp(`^<@!?${bot.user.id}>( |)$`))) { // bot mentionned in the message
+        message.channel.send(bot.language.MENTION_BOT.replace(/<name>/g, bot.user.username)
+                                                     .replace(/<prefix>/g, prefix));
 
-    if(!message.content.startsWith(prefix)) { return }
+    } else if (message.content.startsWith(prefix)) { // bot prefix used
+        const command = message.content.split(' ')[0].slice(prefix.length).toLowerCase();
+        const args = message.content.split(' ').slice(1);
+        let cmd;
 
-    const command = message.content.split(' ')[0].slice(prefix.length).toLowerCase();
-    const args = message.content.split(' ').slice(1);
-    let cmd;
+        if (bot.commandes.has(command)) {
+            cmd = bot.commandes.get(command);
+        } else if (bot.aliases.has(command)) {
+            cmd = bot.commandes.get(bot.aliases.get(command));
+        }
+        if(!cmd) return;
+        
+        const props = require(`../command/${cmd.conf.dir}/${cmd.conf.name}`);
+        
+        // COOLDOWNS
+        if (!cooldowns.has(props.conf.name)) {
+            cooldowns.set(props.conf.name, new Collection()); // cooldown creation
+        }
+        
+        const now = Date.now();
+        const timestamps = cooldowns.get(props.conf.name);
+        const cooldownAmount = (props.conf.cooldown || 1) * 1000;
+        
+        if (timestamps.has(message.author.id)) {
+            let expirationTime = timestamps.get(message.author.id) + cooldownAmount;
 
-    if (bot.commandes.has(command)) {
-        cmd = bot.commandes.get(command);
-    } else if (bot.aliases.has(command)) {
-        cmd = bot.commandes.get(bot.aliases.get(command));
-    }
-    if(!cmd) return;
-    
-    const props = require(`../command/${cmd.conf.dir}/${cmd.conf.name}`);
-    
-    // COOLDOWNS & ERRORS
-    if (!cooldowns.has(props.conf.name)) {
-        cooldowns.set(props.conf.name, new Collection());
-    }
-    
-    const now = Date.now();
-    const timestamps = cooldowns.get(props.conf.name);
-    const cooldownAmount = (props.conf.cooldown || 1) * 1000;
-    
-    if (timestamps.has(message.author.id)) {
-        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-    if (now < expirationTime) {
-        const timeLeft = (expirationTime - now) / 1000;
-        return message.channel.send(bot.language.COOLDOWN_ERR.replace(/<time>/g, timeLeft.toFixed(1))
-                                                             .replace(/<cmd>/g, props.conf.name));
-    }
-    }
-    timestamps.set(message.author.id, now);
-    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+            if (now < expirationTime) {
+                const timeLeft = (expirationTime - now) / 1000;
+                return message.channel.send(bot.language.COOLDOWN_ERR.replace(/<time>/g, timeLeft.toFixed(1))
+                                                                     .replace(/<cmd>/g, props.conf.name));
+            }
+        } else {
+            timestamps.set(message.author.id, now); // addition of the cooldown to the user
+        }
+        setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
 
-    // LOADING COMMANDS
-    try {
-        cmd.run(bot, message, args);
-    } catch (e) {
-        bot.emit("error", e, message);
+        // LOADING COMMANDS
+        try {
+            cmd.run(bot, message, args);
+        } catch (e) {
+            bot.emit("error", e, message);
+        }
     }
 };
--- utils/logger.js
+++ utils/logger.js
@@ -3,33 +3,35 @@ const moment = require("moment");
 const { LoggerError } = require('./CustomError');
 const timestamp = `[${moment().format(" HH:mm:ss | DD-MM-YYYY")}]`;
 
-  function log(content){
-  if(!content) throw new LoggerError('Aucun texte trouvé');
+const noTextFound = "No text found";
+
+function log(content){
+  if(!content) throw new LoggerError(noTextFound);
   console.log(`${chalk.cyan(timestamp)} ${chalk.blue.underline(('[LOG]'))} ${content}`)
 }
 
 function loader(content){
-  if(!content) throw new LoggerError('Aucun texte trouvé');
+  if(!content) throw new LoggerError(noTextFound);
   console.log(`${chalk.cyan(timestamp)} ${chalk.green.underline(('[LOADER]'))} ${content}`)
 }
 
 function error(content){
-  if(!content) throw new LoggerError('Aucun texte trouvé');
+  if(!content) throw new LoggerError(noTextFound);
   console.log(`${chalk.cyan(timestamp)} ${chalk.red.underline(('[ERROR]'))} ${content}`)
 }
 
 function warn(content){
-  if(!content) throw new LoggerError('Aucun texte trouvé');
+  if(!content) throw new LoggerError(noTextFound);
   console.log(`${chalk.cyan(timestamp)} ${chalk.yellow.underline(('[WARN]'))} ${content}`)
 }
 
 function info(content){
-  if(!content) throw new LoggerError('Aucun texte trouvé');
+  if(!content) throw new LoggerError(noTextFound);
   console.log(`${chalk.cyan(timestamp)} ${chalk.magenta.underline(('[INFO]'))} ${content}`)
 }
 
 function database(content){
-  if(!content) throw new LoggerError('Aucun texte trouvé');
+  if(!content) throw new LoggerError(noTextFound);
   console.log(`${chalk.cyan(timestamp)} ${chalk.yellowBright.underline(('[DATABASE]'))} ${content}`)
 }
 
